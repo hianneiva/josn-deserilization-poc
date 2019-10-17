20d7519b88eab4bba521a37357ed538fdb634a3a
gemah <hianneiva@gmail.com>
2019-10-17 10:10:21 -0300

    PoC upload

---
+++ CSClassFromJSON/CSClassFromJSON/Program.cs
@@ -0,0 +1,15 @@
+﻿using QuickTypeGetter;
+using System;
+using System.IO;
+
+namespace CSClassFromJSON
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Root payload = Root.FromJson(File.ReadAllText("..\\..\\..\\payload.json"));
+            Console.WriteLine(payload.ToString());
+        }
+    }
+}
